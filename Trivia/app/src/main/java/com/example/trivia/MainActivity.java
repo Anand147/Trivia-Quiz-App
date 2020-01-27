1e3f094c1cd07f021ece5f6b1c54c72f21facedb
Anand-Computer <ananddeshmukh303@gmail.com>
2020-01-27 21:04:07 +0530

    Complete

---
+++ Trivia/app/src/main/java/com/example/trivia/MainActivity.java
@@ -0,0 +1,214 @@
+package com.example.trivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.trivia.data.AnswerListAsyncResponse;
+import com.example.trivia.data.QuestionBank;
+import com.example.trivia.model.Question;
+import com.example.trivia.model.Score;
+import com.example.trivia.util.Prefs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView questionTextView;
+    private TextView questionCounterTextView;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private int currentQuestionIndex = 0;
+    private List<Question> questionList;
+
+    private TextView scoreTextView;
+    private TextView highestScoerTextView;
+    private int scoreCounter = 0;
+    private Score score;
+
+    private Prefs prefs;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        score = new Score();
+        scoreTextView = findViewById(R.id.scoreTotal);
+        highestScoerTextView = findViewById(R.id.scoreHighest);
+
+        prefs = new Prefs(MainActivity.this);
+
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionCounterTextView = findViewById(R.id.counter_question);
+        questionTextView = findViewById(R.id.question_textview);
+
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+        highestScoerTextView.setText("Highest Score : " + prefs.getHighestScore());
+
+        currentQuestionIndex = prefs.getState();
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+                questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+                questionCounterTextView.setText(currentQuestionIndex  + "/" + questionList.size());
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.prev_button:
+                if (currentQuestionIndex > 0){
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionList.size();
+                updateQuestion();
+                }
+                break;
+            case R.id.next_button:
+                if (currentQuestionIndex != questionList.size()){
+                goNext();
+                }
+                break;
+            case R.id.true_button:
+                checkAnswer(true);
+                updateQuestion();
+                break;
+            case R.id.false_button:
+                checkAnswer(false);
+                updateQuestion();
+                break;
+        }
+    }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answerIsTrue = questionList.get(currentQuestionIndex).getAnswerTrue();
+        int toastMessageId = 0;
+        if(userChooseCorrect == answerIsTrue){
+            fadeView();
+            addPoints();
+            toastMessageId = R.string.correct_answer;
+        }else{
+            subPoints();
+            shakeAnimation();
+            toastMessageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this, toastMessageId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void addPoints(){
+        scoreCounter += 5;
+        score.setScore(scoreCounter);
+        scoreTextView.setText("Total Score : " + score.getScore());
+    }
+
+    private void subPoints(){
+        scoreCounter -= 5;
+        if (scoreCounter > 0){
+            score.setScore(scoreCounter);
+            scoreTextView.setText("Total Score : " + score.getScore());
+        }else {
+            scoreCounter = 0;
+            score.setScore(scoreCounter);
+            scoreTextView.setText("Total Score : " + score.getScore());
+        }
+    }
+
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextView.setText(question);
+        questionCounterTextView.setText(currentQuestionIndex + "/" + questionList.size());
+    }
+
+    private void fadeView(){
+        final CardView cardView = findViewById(R.id.cardView);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+
+        alphaAnimation.setDuration(350);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(Color.WHITE);
+                goNext();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void shakeAnimation(){
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake_animation);
+        final CardView cardView = findViewById(R.id.cardView);
+        cardView.setAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(Color.WHITE);
+                goNext();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        prefs.saveHighestScore(scoreCounter);
+        prefs.setState(currentQuestionIndex);
+        super.onPause();
+    }
+
+    private void goNext(){
+        currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+        updateQuestion();
+    }
+
+
+}
